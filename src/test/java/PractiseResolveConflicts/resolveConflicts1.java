e4b23a4854d73e8b4539a991801e6c330e836b53
Divya-Sai <divya.bugathaa@gmail.com>
2022-09-21 10:31:32 +0100

    added resolveconflict Page 1

---
+++ src/test/java/PractiseResolveConflicts/resolveConflicts1.java
@@ -0,0 +1,14 @@
+package PractiseResolveConflicts;
+
+public class resolveConflicts1 {
+
+    //printing numbers from 1 to 10
+    public void printingvalues(){
+
+        for(int i=0;i<=10;i++){
+            System.out.println(i);
+        }
+
+    }
+
+}
